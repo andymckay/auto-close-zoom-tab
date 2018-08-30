e7219a7b44af51c334dc4eda55956d5936665212
Andy McKay <andymckay@github.com>
2018-08-30 14:34:49 -0700

    better regex

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 function maybeClose(tabId, changeInfo, tab) {
-	if (tab.url.match(/https:\/\/([a-z]+).zoom.us\/j\/([0-9]+)\?pwd=\&status=success/)) {
+	if (tab.url.match(/https:\/\/([a-z]+).zoom.us\/[a-z]\/([0-9]+)\?(.*)?status=success/)) {
 		setTimeout(function() {
 			browser.tabs.remove(tab.id);
 		}, 2000);
