9d2d9d435bcbef88f4940182c0f26b24dd1a4ca8
Daniel Blackhall <github@blerk.info>
2020-06-17 17:47:21 +1000

    Close postattendee tabs as well

--- background.js
+++ background.js
@@ -1,6 +1,7 @@
 function maybeClose(tabId, changeInfo, tab) {
 	// example url: https://foo.zoom.us/j/634874123#success
-	if (tab.url.match(/https:\/\/(?:[a-z0-9_]+[.])?zoom.us\/[a-z]\/[0-9]+\/?[?#](?:.*status=)?success/)) {
+	if (tab.url.match(/https:\/\/(?:[a-z0-9_]+[.])?zoom.us\/[a-z]\/[0-9]+\/?[?#](?:.*status=)?success/) ||
+			tab.url.match(/https:\/\/[a-z]+\.zoom\.us\/postattendee.*/)) {
 		setTimeout(function() {
 			browser.tabs.remove(tab.id);
 		}, 2000);
