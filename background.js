53d76f0525da3f8eff43bb5ca0c839d61cf47587
dan <dan@zumper.com>
2020-07-22 09:13:11 -0700

    allow for passwords in url

--- background.js
+++ background.js
@@ -1,6 +1,7 @@
 function maybeClose(tabId, changeInfo, tab) {
 	// example url: https://foo.zoom.us/j/634874123#success
-	if (tab.url.match(/https:\/\/(?:[a-z0-9_]+[.])?zoom.us\/[a-z]\/[0-9]+\/?[?#](?:.*status=)?success/)) {
+	// example url: https://foo.zoom.us/j/634874123?pwd=fodbarbaz#success
+	if (tab.url.match(/https:\/\/(?:[a-z0-9_]+[.])?zoom.us\/[a-z]\/[0-9]+\/?(?:[?]pwd=[^#&?]+)?[?#]+success/)) {
 		setTimeout(function() {
 			browser.tabs.remove(tab.id);
 		}, 2000);
